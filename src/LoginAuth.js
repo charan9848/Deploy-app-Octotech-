25d35a7ee03abd01e1584d09e16f90395ff3dd45
GANNOJ BHANU CHARAN <gannojcharan@gmail.com>
2024-06-29 17:24:23 +0530

    firebase update

--- src/LoginAuth.js
+++ src/LoginAuth.js
@@ -20,10 +20,13 @@ function LoginAuth() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    // Validate email and password
     if (!email || !password) {
       setAlertMessage("Email and password are required");
+      return;
+    }
+
+    if (password.length <= 5) {
+      setAlertMessage("Password must be more than 5 characters.");
       return;
     }
 
@@ -34,17 +37,21 @@ function LoginAuth() {
       window.location.href = "/Dashboard";
     } catch (err) {
       console.error(err);
+      console.log(err.code);
 
-      // Handle specific errors and set alert message accordingly
       switch (err.code) {
         case "auth/invalid-email":
           setAlertMessage("Email is not valid");
           break;
-        case "auth/wrong-password":
-          setAlertMessage("Password is incorrect");
+        case "auth/user-disabled":
+          setAlertMessage("User account is disabled");
           break;
         case "auth/user-not-found":
-          setAlertMessage("User not found");
+          setAlertMessage("Email not found");
+          break;
+        case "auth/wrong-password":
+        case "auth/invalid-credential":
+          setAlertMessage("Wrong password");
           break;
         default:
           setAlertMessage("An error occurred");
@@ -58,20 +65,19 @@ function LoginAuth() {
         <MDBContainer fluid>
           <MDBRow className='d-flex justify-content-center align-items-center h-100'>
             <MDBCol col='12'>
-              <MDBCard className='bg-dark text-white my-5 mx-auto' style={{ borderRadius: '1rem', maxWidth: '400px' }}>
+              <MDBCard className='bg-light text-dark my-5 mx-auto' style={{ borderRadius: '1rem', maxWidth: '400px' }}>
                 <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
                   <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
-                  <p className="text-white-50 mb-5">Please enter your login and password!</p>
+                  <p className="text-dark-50 mb-5">Please enter your login and password!</p>
                   {alertMessage && <p className="text-danger mb-3 pb-lg-2">{alertMessage}</p>}
-                  <MDBInput wrapperClass='mb-4 mx-5 w-100 ' labelClass='text-white' label='Email address' name='email' value={email} id='formControlLg' type='email' size="lg" onChange={changeHandler} />
-                  <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Password' name='password' value={password} id='formControlLg' type='password' size="lg" onChange={changeHandler} />
-                  <p className="small mb-3 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p>
-                  <MDBBtn outline className='mx-2 px-5' color='white' size='lg' type='Submit'>
+                  <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-dark' label='Email address' name='email' value={email} type='email' size="lg" onChange={changeHandler} />
+                  <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-dark' label='Password' name='password' value={password} type='password' size="lg" onChange={changeHandler} />
+                  <MDBBtn outline className='mx-2 px-5' color='black' size='lg' type='Submit'>
                     Login
                   </MDBBtn>
                   <br />
                   <div>
-                    <Link to='/signup'><p className="mb-0">Don't have an account? SignUp here</p></Link>
+                    <Link to='/signup'><p className="mb-0">Don't have an account? Sign up here</p></Link>
                   </div>
                 </MDBCardBody>
               </MDBCard>
